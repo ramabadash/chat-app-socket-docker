569ded957d86f4f87a76e24a75883eb5ff315ee3
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-30 19:06:40 +0200

    # Add basic Login component

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h1>Welcome to the chat:</h1>
+      <input placeholder='Enter your user name' />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;

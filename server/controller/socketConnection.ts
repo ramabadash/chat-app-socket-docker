2f76a8d4837a80fd6653ffc62d9d97b1ae1f0b7c
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-30 11:12:01 +0200

    # Comments

--- server/controller/socketConnection.ts
+++ server/controller/socketConnection.ts
@@ -8,24 +8,26 @@ import { SocketType } from '../@types/socket/types';
 import { onMessage } from './socketOnMessage';
 import { onDisconnected } from './socketDisconnected';
 
-/***** CONNECTION *****/
 export const onConnection = (socket: SocketType) => {
+  /***** ON CONNECTION *****/
   console.log('connected user');
   const name = socket.handshake.auth.username;
 
-  USERS.push({ id: socket.id, name });
+  USERS.push({ id: socket.id, name }); // Update users list
   console.log(USERS);
 
-  // Update users about the login
+  // Update users about the login by message
   socket.broadcast.emit('replay', {
     name,
     message: 'Enter to chat',
   });
-  // Send user activity details
-  io.emit('userActivity', USERS);
 
+  io.emit('userActivity', USERS); // Send user activity details
+
+  /***** ON MESSAGE *****/
   socket.on('message', onMessage); // On message event reply to the client
 
+  /***** ON DISCONNECTION *****/
   socket.on('disconnect', () => {
     onDisconnected(name, socket); // On user disconnecting
   });

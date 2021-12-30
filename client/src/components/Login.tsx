a2c0d2042802b410a39757c07a1bade6f8a56738
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-30 19:27:44 +0200

    # Set username on state on the Login component

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// Redux
+import { useAppDispatch } from '../app/hooks';
+// Actions
+import { userLogin } from '../reducers/chatReducer';
 
 function Login() {
+  /***** STATE *****/
+  const [username, setUsername] = useState('');
+
+  /***** FUNCTIONS *****/
+  const dispatch = useAppDispatch();
+
+  const navigate = useNavigate();
+
+  const setUserName = () => {
+    dispatch(userLogin({ username }));
+    navigate('/chat');
+  };
+
   return (
     <div>
       <h1>Welcome to the chat:</h1>
-      <input placeholder='Enter your user name' />
-      <button>Login</button>
+      <input
+        value={username}
+        placeholder='Enter your user name'
+        onChange={e => {
+          setUsername(e.target.value);
+        }}
+      />
+      <button onClick={setUserName}>Login</button>
     </div>
   );
 }

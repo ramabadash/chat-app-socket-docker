9a68e95c0b1d81b1b6710749c33f552a84df1137
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-31 14:21:55 +0200

    # Auto scroll down on the chat area

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { nanoid } from 'nanoid';
 /***** REDUX *****/
 import { useAppSelector } from '../app/hooks';
@@ -8,6 +8,9 @@ import '../styles/Chat.css';
 /* ---------------------- COMPONENT ----------------------  */
 
 function Chat() {
+  /***** REFS *****/
+  const messageEl = useRef<HTMLDivElement | null>(null);
+
   /***** STATE *****/
   const chat = useAppSelector(({ chatReducer }) => chatReducer.chat);
   const username = useAppSelector(({ chatReducer }) => chatReducer.username);
@@ -15,21 +18,39 @@ function Chat() {
     ({ chatReducer }) => chatReducer.typingUser
   );
 
+  /***** EFFECT *****/
+  //Scroll down the massages list on new massages
+  useEffect(() => {
+    if (messageEl.current !== null) {
+      messageEl.current.addEventListener('DOMNodeInserted', event => {
+        messageEl.current!.scroll({
+          top: messageEl.current!.scrollHeight,
+          behavior: 'smooth',
+        });
+      });
+    }
+  }, []);
+
   return (
-    <div className='chat'>
-      <ul className='chat-list'>
-        {chat.map(({ name, message, timeStamp }) => (
-          <li key={nanoid()} className={`${username === name ? 'me' : 'you'}`}>
-            <div className='entete'>
-              <span className={'status green'}></span>
-              <h2>{name}</h2> <h3>{timeStamp}</h3>
-            </div>
-            <div className='message'>{message}</div>
-          </li>
-        ))}
-      </ul>
+    <>
       <div>{typingUser ? <p>{typingUser} is typing ...</p> : ''}</div>
-    </div>
+      <div className='chat' ref={messageEl}>
+        <ul className='chat-list'>
+          {chat.map(({ name, message, timeStamp }) => (
+            <li
+              key={nanoid()}
+              className={`${username === name ? 'me' : 'you'}`}
+            >
+              <div className='entete'>
+                <span className={'status green'}></span>
+                <h2>{name}</h2> <h3>{timeStamp}</h3>
+              </div>
+              <div className='message'>{message}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 

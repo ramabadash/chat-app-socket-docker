8c935f78e7257eea306d800f9a3aa931a4dacd50
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-31 23:53:20 +0200

    # Remove yourself from the users list

--- client/src/components/UsersList.tsx
+++ client/src/components/UsersList.tsx
@@ -22,26 +22,30 @@ function UsersList() {
   return (
     <div className='users-list-container'>
       <ul>
-        {connectedUsers.map(({ id, name }) => (
-          <li
-            key={id}
-            onClick={() => {
-              dispatch(setMessageDestination({ room: id }));
-            }}
-          >
-            <img
-              src='https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png'
-              alt='user'
-            />
-            <div>
-              <h2>{username === name ? '(You)' : name}</h2>
-              <h3>
-                <span className='status green'></span>
-                online
-              </h3>
-            </div>
-          </li>
-        ))}
+        {connectedUsers.map(({ id, name }) => {
+          if (username !== name) {
+            return (
+              <li
+                key={id}
+                onClick={() => {
+                  dispatch(setMessageDestination({ room: id }));
+                }}
+              >
+                <img
+                  src='https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png'
+                  alt='user'
+                />
+                <div>
+                  <h2>{name}</h2>
+                  <h3>
+                    <span className='status green'></span>
+                    online
+                  </h3>
+                </div>
+              </li>
+            );
+          }
+        })}
       </ul>
     </div>
   );

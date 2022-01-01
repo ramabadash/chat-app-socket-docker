a071c4ae4c8b86256b854c6ccd35e3dc4780b450
ramabadash <89576646+ramabadash@users.noreply.github.com>
2022-01-01 15:16:24 +0200

    # Build and dockerize server diractiory

---
+++ server/build/controller/socketConnection.js
@@ -0,0 +1,41 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.onConnection = void 0;
+// DB
+const users_1 = __importDefault(require("../db/users"));
+// io
+const server_1 = require("../server");
+// Functions
+const socketOnMessage_1 = require("./socketOnMessage");
+const socketDisconnected_1 = require("./socketDisconnected");
+const onConnection = (socket) => {
+    /***** ON CONNECTION *****/
+    console.log('connected user');
+    const name = socket.handshake.auth.username;
+    users_1.default.push({ id: socket.id, name }); // Update users list
+    console.log(users_1.default);
+    // Update users about the login by message
+    socket.broadcast.emit('replay', {
+        name,
+        message: 'Enter to the chat',
+    });
+    server_1.io.emit('userActivity', users_1.default); // Send user activity details
+    /***** ON MESSAGE *****/
+    // On message event reply to the client
+    socket.on('message', ({ name, message, to }) => {
+        const messageObj = { name, message, to };
+        (0, socketOnMessage_1.onMessage)(messageObj, socket.id);
+    });
+    /***** ON TYPING *****/
+    socket.on('userTyping', ({ name, type }) => {
+        server_1.io.emit('userTypingReplay', { name, type });
+    });
+    /***** ON DISCONNECTION *****/
+    socket.on('disconnect', () => {
+        (0, socketDisconnected_1.onDisconnected)(name, socket); // On user disconnecting
+    });
+};
+exports.onConnection = onConnection;
